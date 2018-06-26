120604a2ef5ec0528e1e55c40ff55aeabee05d19
BibekStha123 <34579460+BibekStha123@users.noreply.github.com>
2018-06-26 10:35:10 +0545

    Add files via upload

---
+++ People.java
@@ -0,0 +1,45 @@
+package TextConvert;
+
+public class People {
+
+	private String name;
+	private int  roll;
+	private String profession;
+	private String address;
+	
+	public void setNmae(String name)
+	{
+		this.name=name;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
